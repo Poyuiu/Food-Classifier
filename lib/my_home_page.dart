28d4a5c0b22924c24b0b93f73de5d5ce9e3d7636
Poyu Juan <yoyohihirobert@gmail.com>
2023-01-05 20:53:10 +0800

    feat: picking image

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -14,9 +14,31 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool _hasRunModel = false;
-  File? image;
+  File? _image;
   List? _result;
   final _picker = ImagePicker();
+
+  // TODOTODOTODO
+  // OUR MODEL DISPLAY
+  // void foodClassifier(final File image)async{
+  //   var result = await 
+  // }
+
+  void pickCameraImage() async {
+    var image = await _picker.pickImage(source: ImageSource.camera);
+    if (image == null) return null;
+    setState(() {
+      _image = File(image.path);
+    });
+  }
+
+  void pickGalleryImage() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _image = File(image.path);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,12 +53,81 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _body(final BuildContext context) => Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(children: [
-          _mediumVerticalSpacer(),
-        ]),
+        child: Column(
+          children: [
+            _mediumVerticalSpacer(),
+            _hasRunModel
+                ? Column(
+                    children: [
+                      SizedBox(
+                        height: 300,
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.file(_image!),
+                      ),
+                      _mediumVerticalSpacer(),
+                      Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                        ),
+                        child: Text(
+                          '${_result![0]['label']}',
+                          style: const TextStyle(
+                            color: Colors.purple,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                : const Text(
+                    'Food Classifier',
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+            _selectionButtons()
+          ],
+        ),
       );
 
   Widget _mediumVerticalSpacer() => const SizedBox(
         height: 20,
+      );
+
+  Widget _selectionButtons() => Column(
+        children: [
+          _mediumVerticalSpacer(),
+          _selectionPhoto('Capture Photo', () {}),
+          _mediumVerticalSpacer(),
+          _selectionPhoto('Select Photo', () {}),
+        ],
+      );
+
+  Widget _selectionPhoto(final String label, final VoidCallback onTap) =>
+      Column(
+        children: [
+          GestureDetector(
+            onTap: onTap,
+            child: Container(
+              width: 150,
+              padding: const EdgeInsets.all(8),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Text(
+                label,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
+        ],
       );
 }
