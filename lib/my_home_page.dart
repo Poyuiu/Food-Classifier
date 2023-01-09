65d48c99e0acc6c0326a5586ec93b5fa6a1c41f1
Poyu Juan <yoyohihirobert@gmail.com>
2023-01-10 03:25:02 +0800

    feat: add pop up info

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -20,19 +21,47 @@ class _MyHomePageState extends State<MyHomePage> {
   List? _result;
   final _picker = ImagePicker();
   // res, len is for debugging
-  String? _res;
   int? _len;
-  String _cannot = 'Cannot Detect It';
+  final String _cannot = 'Cannot Detect It';
 
+  final List<List<double>> _foodInfo = [
+    [482, 10.3, 14.9, 77, 0.832, 65],
+    [533, 12, 26.6, 61.4, 1.29, 79],
+    [542, 12.9, 28.3, 59, 0.739, 69],
+    [620, 16.8, 19.1, 95.2, 1.012, 79],
+    [459, 9.6, 11.8, 78.7, 0.755, 68],
+    [575, 12.4, 17, 93.2, 0.965, 79],
+    [537, 14.1, 27.4, 58.7, 0.869, 69],
+    [454, 16.1, 15, 63.8, 1.15, 79],
+    [615, 15.8, 34, 61.4, 0.932, 69],
+    [442, 17.8, 14.2, 60.7, 0.822, 79],
+    [531, 10.7, 20.1, 76.7, 0.735, 65],
+    [500, 14.4, 23.1, 58.6, 0.928, 79],
+    [375, 23.5, 24.8, 14.6, 1.108, 69]
+  ];
+
+  final List<String> _foodName = [
+    '義美蝦仁炒飯',
+    '青醬野菇義大利麵',
+    '青醬蛤蠣義大利麵',
+    '素三杯炒飯',
+    '素三杯嫩菇拌飯',
+    '薑黃素炒飯',
+    '奶油培根義大利麵',
+    '雙醬雞肉義大利麵',
+    '番茄肉醬義大利麵',
+    '番茄牛肉義大利麵',
+    '義美鮭魚炒飯',
+    '桂冠培根義大利麵',
+    '麻辣臭豆腐'
+  ];
   // TODOTODOTODO
   // OUR MODEL DISPLAY
 
   void loadModel() async {
     String? res = await Tflite.loadModel(
         model: 'assets/model_unquant2.tflite', labels: 'assets/labels2.txt');
-    setState(() {
-      _res = res;
-    });
+    setState(() {});
   }
 
   @override
@@ -148,12 +177,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
             _mediumVerticalSpacer(),
             _hasRunModel
-                ? Text('${_len == 0 ? _cannot : _result![0]['label']}',
-                    style: const TextStyle(
-                      color: Colors.purple,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ))
+                ? _len == 0
+                    ? Text(_cannot,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ))
+                    : _displayFoodInfo(int.parse(_result![0]['label']))
                 : _mediumVerticalSpacer(),
           ],
         ),
@@ -195,5 +226,83 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           )
         ],
+      );
+
+  Widget _displayFoodInfo(final int item) => TextButton(
+        // style:
+            // ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.deepPurple),),
+        onPressed: () => showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            // contentPadding: const EdgeInsets.only(left: 10.0, right: 10.0),
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            title: Text(
+              _foodName[item],
+              style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            content: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                //position
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    '熱量： ${_foodInfo[item][0]}卡',
+                    style: const TextStyle(fontSize: 24, color: Colors.indigo),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '脂肪： ${_foodInfo[item][2]}克',
+                    style: const TextStyle(fontSize: 24, color: Colors.indigo),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '碳水： ${_foodInfo[item][3]}克',
+                    style: const TextStyle(fontSize: 24, color: Colors.indigo),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '鈉含量： ${_foodInfo[item][4]}克',
+                    style: const TextStyle(fontSize: 24, color: Colors.indigo),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '蛋白質： ${_foodInfo[item][1]}克',
+                    style: const TextStyle(fontSize: 24, color: Colors.indigo),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '價格： ${_foodInfo[item][5]}元',
+                    style: const TextStyle(fontSize: 24, color: Colors.indigo),
+                  )
+                ]),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'OK'),
+                child: const Text(
+                  'Nice',
+                  style: TextStyle(fontSize: 20, color: Colors.blue),
+                ),
+              ),
+            ],
+          ),
+        ),
+        child: const Text(
+          'More Information',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.deepPurple),
+        ),
       );
 }
